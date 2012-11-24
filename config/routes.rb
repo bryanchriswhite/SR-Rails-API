d34d6e3fc74cdba33900fd26609376488acbaf09
Bryan White <bryanchriswhite@gmail.com>
2012-11-24 07:30:07 -0500

    removed index.html and added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SecretrevApi::Application.routes.draw do
   resources :mods, except: :edit
 
+  root :to => 'mods#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
