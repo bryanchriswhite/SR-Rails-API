340474e8e36339e774d2b05a5f4a204547ec3a15
Bryan White <bryanchriswhite@gmail.com>
2012-12-09 16:54:19 -0500

    fix categories_controller to respond properly to JSONP requests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.all
-    render json: @categories
+    render json: @categories, callback: params[:callback]
   end
 end
