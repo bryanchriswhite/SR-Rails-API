4505445369b0b46661ae7789ffaa717d4ca454e5
Bryan White <bryanchriswhite@gmail.com>
2012-12-09 22:37:14 -0500

    fix mods_controller to respond properly to JSONP requests

--- app/controllers/mods_controller.rb
+++ app/controllers/mods_controller.rb
@@ -1,7 +1,7 @@
 class ModsController < ApplicationController
   def index
     @mods = Mod.all
-    render json: @mods
+    render json: @mods, callback: params[:callback]
   end
 
   def show
@@ -11,26 +11,26 @@ class ModsController < ApplicationController
 
   def name
     @mods = Mod.where "name like ?", "%#{params[:q]}%"
-    render json: @mods
+    render json: @mods, callback: params[:callback]
   end
 
   def version
     @mods = Mod.where "minecraft_version like ?", "%#{params[:q]}%"
-    render json: @mods
+    render json: @mods, callback: params[:callback]
   end
 
   def author
     @mods = Mod.where "author like ?", "%#{params[:q]}%"
-    render json: @mods
+    render json: @mods, callback: params[:callback]
   end
 
   def count
     @mods = Mod.limit(params[:count]).offset(params[:offset])
-    render json: @mods
+    render json: @mods, callback: params[:callback]
   end
 
   def total
     @total = Mod.all.length
-    render json: @total
+    render json: @total, callback: params[:callback]
   end
 end
