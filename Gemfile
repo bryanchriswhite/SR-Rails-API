7290aaa7b16fc54f97149e46e8699dc3a7bf5a65
Nembus <timurlan77@gmail.com>
2012-12-16 21:03:55 -0500

    Using RSpec instead of Test

--- Gemfile
+++ Gemfile
@@ -14,7 +14,10 @@ gem 'pry'
 gem 'awesome_print'
 
 
-# To use ActiveModel has_secure_password
+gem "rspec-rails", ">= 2.11.4", :group => [:development, :test]
+gem "database_cleaner", ">= 0.9.1", :group => :test
+gem "capybara"
+gem "factory_girl_rails", ">= 4.1.0", :group => [:development, :test]# To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
 # To use Jbuilder templates for JSON
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121209025703) do
+ActiveRecord::Schema.define(:version => 20121216234819) do
+
+  create_table "api_keys", :force => true do |t|
+    t.string   "access_token"
+    t.integer  "user_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -25,6 +32,14 @@ ActiveRecord::Schema.define(:version => 20121209025703) do
     t.integer "mod_id"
   end
 
+  create_table "categorizations", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "mod_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
   create_table "mods", :force => true do |t|
     t.string   "name"
     t.string   "minecraft_version"
@@ -32,6 +47,15 @@ ActiveRecord::Schema.define(:version => 20121209025703) do
     t.string   "author"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.string   "email"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
