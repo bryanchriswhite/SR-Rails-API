7a19c6b03cbda9760fe1e386f5d03932a9e3a2a7
Bryan White <bryanchriswhite@gmail.com>
2013-01-17 08:15:29 -0500

    fix "Access-Control-Allow-Headers" not allowing "X-Requested-With" and "Content-Type"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,6 +25,7 @@ class ApplicationController < ActionController::API
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Content-Type'
     headers['Access-Control-Max-Age'] = "1728000"
   end
 
